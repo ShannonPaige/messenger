c1c55f191b31f44432910163d786b753998b6dcc
ShannonPaige <spaige@gmail.com>
2017-07-10 15:22:08 -0600

    refactor test

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -1,19 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe "users/show.html.erb", type: :view do
+  before(:all) do
+    @aliceID = User.find_by_name("Alice").id
+  end
+
   it "allows a user to get to their homepage" do
     visit '/'
     fill_in "Name", with: "Alice"
     fill_in "Password", with: "password"
     click_button "Login"
 
+    visit "/users/#{@aliceID}"
+
     expect(page).to have_content 'Welcome Alice'
   end
 
-  it "does not allow other users to get to a user's homepage" do
-    visit '/users/7'
+  it "does not allow guests to get to a user's homepage" do
 
-    expect(page).to have_content 'Unauthorized to visit this page'
+    visit '/'
+    fill_in "Name", with: "Bob"
+    fill_in "Password", with: "password"
+    click_button "Login"
+
+    visit "/users/#{@aliceID}"
+
+    assert page.has_content?("Welcome Bob")
+  end
+
+  it "does not allow other users to get to a user's homepage" do
+    visit "/users/#{@aliceID}"
+
     assert page.has_content?("Unauthorized to visit this page")
     assert_equal login_path, current_path
   end
