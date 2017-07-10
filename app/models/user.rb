6f998311368593d4480a5bc1b6d7fce8bd98c906
ShannonPaige <spaige@gmail.com>
2017-07-10 16:37:59 -0600

    add message model and relation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
+  has_many :messages_as_sender, class_name: "Message", foreign_key: 'sender_id'
+  has_many :messages_as_receiver, class_name: "Message", foreign_key: 'receiver_id'
+
   has_secure_password
+
 
   def self.all_except(user)
     where.not(id: user.id)
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170710180714) do
+ActiveRecord::Schema.define(version: 20170710212351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.string "body"
+    t.bigint "sender_id"
+    t.bigint "receiver_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["receiver_id"], name: "index_messages_on_receiver_id"
+    t.index ["sender_id"], name: "index_messages_on_sender_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
